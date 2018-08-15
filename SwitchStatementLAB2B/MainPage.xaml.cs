a860d0b2dc491b0feb6176c417b0fa9d02990543
Auston Hefling <auston.hefling@outlook.com>
2018-08-15 07:52:12 -0400

    Finished SwitchStatements LAB2B

--- SwitchStatementLAB2B/MainPage.xaml.cs
+++ SwitchStatementLAB2B/MainPage.xaml.cs
@@ -13,9 +13,9 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace SwitchStatementLAB2B
+namespace SwitchStatement
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -25,6 +25,42 @@ namespace SwitchStatementLAB2B
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void copyClick(object sender, RoutedEventArgs e)
+        {
+            target.Text = "";
+            string from = source.Text;
+            for (int i = 0; i != from.Length; i++)
+            {
+                char current = from[i];
+                copyOne(current);
+            }
+        }
+
+        private void copyOne(char current)
+        {
+            switch (current)
+            {
+                case '<':
+                    target.Text += "&lt";
+                    break;
+                case '>':
+                    target.Text += "&gt;";
+                    break;
+                case '&':
+                    target.Text += "&amp;";
+                    break;
+                case '\"':
+                    target.Text += "&#34;";
+                    break;
+                case '\'':
+                    target.Text += "&#39;";
+                    break;
+                default:
+                    target.Text += current;
+                    break;
+            }
         }
     }
 }
